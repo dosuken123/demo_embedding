35a82440c8d94b65b8ca12350e54f9abbf1886fb
Shinya Maeda <dosuken0122@gmail.com>
2024-08-11 19:27:27 +0900

    gpt model

--- demo_embedding/mode.py
+++ demo_embedding/mode.py
@@ -14,21 +14,35 @@ class GPTConfig:
 
 class SelfAttention(nn.Module):
     def __init__(self, config: GPTConfig):
-        self().__init__()
+        super().__init__()
         self.config = config
 
-        self.qkv_embedding = nn.Embedding(
-            config.embedding_dim, config.embedding_dim * 3
-        )
+        self.qkv_embedding = nn.Linear(config.embedding_dim, config.embedding_dim * 3)
         self.output = nn.Linear(config.embedding_dim, config.embedding_dim)
 
     def forward(self, x):
-        pass
+        B, T, C = x.size()
+
+        qkv = self.qkv_embedding(x)
+        q, k, v = qkv.split(self.config.embedding_dim, dim=2)
+        q = q.view(B, T, self.config.head_size, C // self.config.head_size).transpose(
+            1, 2
+        )
+        k = k.view(B, T, self.config.head_size, C // self.config.head_size).transpose(
+            1, 2
+        )
+        v = v.view(B, T, self.config.head_size, C // self.config.head_size).transpose(
+            1, 2
+        )
+        out = torch.nn.functional.scaled_dot_product_attention(q, k, v, is_causal=True)
+        out = out.transpose(1, 2).contiguous().view(B, T, C)
+        out = self.output(out)
+        return out
 
 
 class MLP(nn.Module):
     def __init__(self, config: GPTConfig):
-        self().__init__()
+        super().__init__()
         self.config = config
 
         self.input = nn.Linear(config.embedding_dim, config.embedding_dim * 4)
@@ -36,12 +50,15 @@ class MLP(nn.Module):
         self.output = nn.Linear(config.embedding_dim * 4, config.embedding_dim)
 
     def forward(self, x):
-        pass
+        x = self.input(x)
+        x = self.activation(x)
+        x = self.output(x)
+        return x
 
 
 class AttentionLayer(nn.Module):
     def __init__(self, config: GPTConfig):
-        self().__init__()
+        super().__init__()
 
         self.self_attention_norm = nn.LayerNorm(config.embedding_dim)
         self.self_attention = SelfAttention(config)
@@ -49,15 +66,17 @@ class AttentionLayer(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        pass
+        x = x + self.self_attention(self.self_attention_norm(x))
+        x = x + self.mlp(self.mlp_norm(x))
+        return x
 
 
 class GPT(nn.Module):
     def __init__(self, config: GPTConfig):
-        self().__init__()
+        super().__init__()
         self.config = config
 
-        self.word_encoding = nn.Embedding(config.vocab_size, config.embedding_dim)
+        self.token_to_embedding = nn.Embedding(config.vocab_size, config.embedding_dim)
         self.positional_encoding = nn.Embedding(
             config.context_size, config.embedding_dim
         )
@@ -65,7 +84,58 @@ class GPT(nn.Module):
             [AttentionLayer(config) for _ in range(config.layer_size)]
         )
         self.decoding_norm = nn.LayerNorm(config.embedding_dim)
-        self.word_decoding = nn.Linear(config.embedding_dim, config.vocab_size)
+        self.embedding_to_token = nn.Linear(config.embedding_dim, config.vocab_size)
 
-    def forward(self, x):
-        pass
+    def forward(self, inputs, targets=None):
+        B, T = inputs.size()
+
+        assert T <= self.config.context_size, "Context size is too large"
+
+        pos = torch.arange(0, T, step=1, dtype=torch.long, device=inputs.device)
+        pos_emb = self.positional_encoding(pos)
+        # print(f"inputs: {inputs}")
+        tok_emb = self.token_to_embedding(inputs)
+        x = tok_emb + pos_emb
+
+        for layer in self.attention_layers:
+            x = layer(x)
+
+        x = self.decoding_norm(x)
+        logits = self.embedding_to_token(x)
+
+        loss = None
+        if targets is not None:
+            loss = torch.nn.functional.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1)
+            )
+
+        return logits, loss
+
+
+####
+
+# inputs = torch.randn(4, 12, 32, dtype=torch.float)
+# print(f"inputs: {inputs.size()}")
+# B, T, C = inputs.size()
+
+# ln = nn.Linear(32, 32 * 3)
+# qkv = ln(inputs)
+# print(f"qkv.size(): {qkv.size()} ")
+
+# q, k, v = qkv.split(32, dim=2)
+# print(f"q.size(): {q.size()}")
+# print(f"k.size(): {k.size()}")
+# print(f"v.size(): {v.size()}")
+# print(f"q: {q[:1,:1,:10]}")
+# print(f"k: {k[:1,:1,:10]}")
+# print(f"v: {v[:1,:1,:10]}")
+
+# config = GPTConfig()
+# model = GPT(config)
+
+# x = torch.randint(0, 50257, (4, 12), dtype=torch.long)
+# y = torch.randint(0, 50257, (4, 12), dtype=torch.long)
+# # print(x)
+# logits, loss = model(x, y)
+# print(f"logits: {logits}")
+# print(f"loss: {loss}")
